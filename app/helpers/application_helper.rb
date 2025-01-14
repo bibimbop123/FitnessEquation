694e8df2c9459709983a9f373ebc19ac9c3f15ad
briankim <brianhkim13@gmail.com>
2025-01-14 11:56:24 -0600

    calculate age and user edit for gender and dob

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def calculate_age(dob)
+    now = Time.now.utc.to_date
+    age = now.year - dob.year
+    age -= 1 if now < dob + age.years # Adjust if the birthday hasn't occurred yet this year
+    age
+  end
 end
