b9fa755fe9df1d735d71cadc3b51a1005649625a
briankim <brianhkim13@gmail.com>
2025-02-08 15:00:22 -0600

    created a posgresql db and running locally

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,144 +10,132 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 20_250_119_215_905) do
+ActiveRecord::Schema[7.1].define(version: 2025_01_19_215905) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'activities', force: :cascade do |t|
-    t.string 'trackable_type'
-    t.bigint 'trackable_id'
-    t.string 'owner_type'
-    t.bigint 'owner_id'
-    t.string 'key'
-    t.text 'parameters'
-    t.string 'recipient_type'
-    t.bigint 'recipient_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index %w[owner_id owner_type], name: 'index_activities_on_owner_id_and_owner_type'
-    t.index %w[owner_type owner_id], name: 'index_activities_on_owner'
-    t.index %w[recipient_id recipient_type], name: 'index_activities_on_recipient_id_and_recipient_type'
-    t.index %w[recipient_type recipient_id], name: 'index_activities_on_recipient'
-    t.index %w[trackable_id trackable_type], name: 'index_activities_on_trackable_id_and_trackable_type'
-    t.index %w[trackable_type trackable_id], name: 'index_activities_on_trackable'
+  create_table "activities", force: :cascade do |t|
+    t.string "trackable_type"
+    t.bigint "trackable_id"
+    t.string "owner_type"
+    t.bigint "owner_id"
+    t.string "key"
+    t.text "parameters"
+    t.string "recipient_type"
+    t.bigint "recipient_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["owner_id", "owner_type"], name: "index_activities_on_owner_id_and_owner_type"
+    t.index ["owner_type", "owner_id"], name: "index_activities_on_owner"
+    t.index ["recipient_id", "recipient_type"], name: "index_activities_on_recipient_id_and_recipient_type"
+    t.index ["recipient_type", "recipient_id"], name: "index_activities_on_recipient"
+    t.index ["trackable_id", "trackable_type"], name: "index_activities_on_trackable_id_and_trackable_type"
+    t.index ["trackable_type", "trackable_id"], name: "index_activities_on_trackable"
   end
 
-  create_table 'ahoy_events', force: :cascade do |t|
-    t.bigint 'visit_id'
-    t.bigint 'user_id'
-    t.string 'name'
-    t.jsonb 'properties'
-    t.datetime 'time'
-    t.index %w[name time], name: 'index_ahoy_events_on_name_and_time'
-    t.index ['properties'], name: 'index_ahoy_events_on_properties', opclass: :jsonb_path_ops, using: :gin
-    t.index ['user_id'], name: 'index_ahoy_events_on_user_id'
-    t.index ['visit_id'], name: 'index_ahoy_events_on_visit_id'
+  create_table "ahoy_events", force: :cascade do |t|
+    t.bigint "visit_id"
+    t.bigint "user_id"
+    t.string "name"
+    t.jsonb "properties"
+    t.datetime "time"
+    t.index ["name", "time"], name: "index_ahoy_events_on_name_and_time"
+    t.index ["properties"], name: "index_ahoy_events_on_properties", opclass: :jsonb_path_ops, using: :gin
+    t.index ["user_id"], name: "index_ahoy_events_on_user_id"
+    t.index ["visit_id"], name: "index_ahoy_events_on_visit_id"
   end
 
-  create_table 'ahoy_visits', force: :cascade do |t|
-    t.string 'visit_token'
-    t.string 'visitor_token'
-    t.bigint 'user_id'
-    t.string 'ip'
-    t.text 'user_agent'
-    t.text 'referrer'
-    t.string 'referring_domain'
-    t.text 'landing_page'
-    t.string 'browser'
-    t.string 'os'
-    t.string 'device_type'
-    t.string 'country'
-    t.string 'region'
-    t.string 'city'
-    t.float 'latitude'
-    t.float 'longitude'
-    t.string 'utm_source'
-    t.string 'utm_medium'
-    t.string 'utm_term'
-    t.string 'utm_content'
-    t.string 'utm_campaign'
-    t.string 'app_version'
-    t.string 'os_version'
-    t.string 'platform'
-    t.datetime 'started_at'
-    t.index ['user_id'], name: 'index_ahoy_visits_on_user_id'
-    t.index ['visit_token'], name: 'index_ahoy_visits_on_visit_token', unique: true
-    t.index %w[visitor_token started_at], name: 'index_ahoy_visits_on_visitor_token_and_started_at'
+  create_table "ahoy_visits", force: :cascade do |t|
+    t.string "visit_token"
+    t.string "visitor_token"
+    t.bigint "user_id"
+    t.string "ip"
+    t.text "user_agent"
+    t.text "referrer"
+    t.string "referring_domain"
+    t.text "landing_page"
+    t.string "browser"
+    t.string "os"
+    t.string "device_type"
+    t.string "country"
+    t.string "region"
+    t.string "city"
+    t.float "latitude"
+    t.float "longitude"
+    t.string "utm_source"
+    t.string "utm_medium"
+    t.string "utm_term"
+    t.string "utm_content"
+    t.string "utm_campaign"
+    t.string "app_version"
+    t.string "os_version"
+    t.string "platform"
+    t.datetime "started_at"
+    t.index ["user_id"], name: "index_ahoy_visits_on_user_id"
+    t.index ["visit_token"], name: "index_ahoy_visits_on_visit_token", unique: true
+    t.index ["visitor_token", "started_at"], name: "index_ahoy_visits_on_visitor_token_and_started_at"
   end
 
-  create_table 'exercises', force: :cascade do |t|
-    t.string 'name'
-    t.integer 'reps'
-    t.integer 'sets'
-    t.float 'weight'
-    t.bigint 'workout_routine_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['workout_routine_id'], name: 'index_exercises_on_workout_routine_id'
+  create_table "exercises", force: :cascade do |t|
+    t.string "name"
+    t.integer "reps"
+    t.integer "sets"
+    t.float "weight"
+    t.bigint "workout_routine_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["workout_routine_id"], name: "index_exercises_on_workout_routine_id"
   end
 
-  create_table 'one_rep_maxes', force: :cascade do |t|
-    t.string 'exercise'
-    t.float 'weight_lbs'
-    t.integer 'reps'
-    t.bigint 'user_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.float 'one_rep_max'
-    t.float 'relative_strength'
-    t.index ['user_id'], name: 'index_one_rep_maxes_on_user_id'
+  create_table "one_rep_maxes", force: :cascade do |t|
+    t.string "exercise"
+    t.float "weight_lbs"
+    t.integer "reps"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.float "one_rep_max"
+    t.float "relative_strength"
+    t.index ["user_id"], name: "index_one_rep_maxes_on_user_id"
   end
 
-  create_table 'snapshots', force: :cascade do |t|
-    t.bigint 'user_id', null: false
-    t.float 'height_cm'
-    t.float 'weight_kg'
-    t.string 'activity_level'
-    t.float 'goal_weight_kg'
-    t.integer 'predicted_time_weeks'
-    t.integer 'calorie_deficit_or_surplus_per_day'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['user_id'], name: 'index_snapshots_on_user_id'
+  create_table "snapshots", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.float "height_cm"
+    t.float "weight_kg"
+    t.string "activity_level"
+    t.float "goal_weight_kg"
+    t.integer "predicted_time_weeks"
+    t.integer "calorie_deficit_or_surplus_per_day"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_snapshots_on_user_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.date 'dob'
-    t.string 'gender'
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.date "dob"
+    t.string "gender"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  create_table 'weightlifting_logs', force: :cascade do |t|
-    t.bigint 'user_id', null: false
-    t.string 'exercise'
-    t.float 'weight'
-    t.integer 'reps'
-    t.integer 'volume'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['user_id'], name: 'index_weightlifting_logs_on_user_id'
+  create_table "workout_routines", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_workout_routines_on_user_id"
   end
 
-  create_table 'workout_routines', force: :cascade do |t|
-    t.string 'name'
-    t.bigint 'user_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['user_id'], name: 'index_workout_routines_on_user_id'
-  end
-
-  add_foreign_key 'exercises', 'workout_routines'
-  add_foreign_key 'one_rep_maxes', 'users'
-  add_foreign_key 'snapshots', 'users'
-  add_foreign_key 'weightlifting_logs', 'users'
-  add_foreign_key 'workout_routines', 'users'
+  add_foreign_key "exercises", "workout_routines"
+  add_foreign_key "one_rep_maxes", "users"
+  add_foreign_key "snapshots", "users"
+  add_foreign_key "workout_routines", "users"
 end
