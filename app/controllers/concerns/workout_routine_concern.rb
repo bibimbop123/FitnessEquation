a40cae1ae3377b2a47253ce9155f8a56e61afc32
briankim <brianhkim13@gmail.com>
2025-03-26 22:04:45 -0500

    added work out routine concern

--- app/controllers/concerns/workout_routine_concern.rb
+++ app/controllers/concerns/workout_routine_concern.rb
@@ -7,6 +7,46 @@ module WorkoutRoutineConcern
     before_action :set_workout_routine, only: %i[show edit update destroy]
   end
 
+  def index
+    @workout_routines = current_user.workout_routines
+  end
+
+  def show; end
+
+  def new
+    @workout_routine = current_user.workout_routines.new
+  end
+
+  def create
+    @workout_routine = current_user.workout_routines.new(workout_routine_params)
+    if @workout_routine.save
+      redirect_to @workout_routine, notice: 'Workout routine was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @workout_routine.update(workout_routine_params)
+      redirect_to @workout_routine, notice: 'Workout routine was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @workout_routine.destroy
+    redirect_to workout_routines_url, notice: 'Workout routine was successfully destroyed.'
+  end
+
+  private
+
+  def workout_routine_params
+    params.require(:workout_routine).permit(:name, :description)
+  end
+
   private
 
   def set_workout_routine
--- app/controllers/workout_routines_controller.rb
+++ app/controllers/workout_routines_controller.rb
@@ -4,43 +4,5 @@ class WorkoutRoutinesController < ApplicationController
   include WorkoutRoutineConcern
   before_action :authenticate_user!
 
-  def index
-    @workout_routines = current_user.workout_routines
-  end
-
-  def show; end
-
-  def new
-    @workout_routine = current_user.workout_routines.new
-  end
-
-  def create
-    @workout_routine = current_user.workout_routines.new(workout_routine_params)
-    if @workout_routine.save
-      redirect_to @workout_routine, notice: 'Workout routine was successfully created.'
-    else
-      render :new
-    end
-  end
-
-  def edit; end
-
-  def update
-    if @workout_routine.update(workout_routine_params)
-      redirect_to @workout_routine, notice: 'Workout routine was successfully updated.'
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @workout_routine.destroy
-    redirect_to workout_routines_url, notice: 'Workout routine was successfully destroyed.'
-  end
-
-  private
-
-  def workout_routine_params
-    params.require(:workout_routine).permit(:name, :description)
-  end
+ 
 end
